bce5412917eabad2b02fb663b3260ce36cbfc51f
Caiopolis <caiohamoreira@gmail.com>
2026-01-27 00:51:34 -0300

    Alterando nome da classe

--- Curso_JavaUdemy/Aula_1/src/Introdução/Primeiro_Programa.java
+++ Curso_JavaUdemy/Aula_1/src/Introdução/Primeiro_Programa.java
@@ -1,9 +1,11 @@
 package Introdução;
 
-public class Inicio {
+public class Primeiro_Programa {
 
 	public static void main(String[] args) {
 		System.out.println("Hellooooou Word");
+		
+		//Clássico "Hellou Word" cumprindo tabela ksks
+		
 	}
-
 }
