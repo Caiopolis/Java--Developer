1ff40c31361e0e2bbd79f8c6fcfdc13339ec5235
Caiopolis <caiohamoreira@gmail.com>
2026-01-28 00:51:24 -0300

    Aula 3 - Scanner Aula 3

---
+++ Curso_JavaUdemy/Aula_3/src/Main.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in); //declarando a variavel que irá ler o input do usuario
+		//Obs: Se trata de uma classe externa necesssario a importação e a instância com new.
+	
+		String x;
+		int y;
+		double z;
+		char w;
+		
+		System.out.println("Digite seu nome:");
+		x = sc.next(); // Atribuindo x como input/scanner (Para texto next())
+		System.out.printf("Olá %s!%n", x);
+		
+		System.out.println("Digite sua idade:");
+		y = sc.nextInt(); // Atribuindo y como input/scanner (Para int nextInt())
+		System.out.printf("Uau %d anos!!%n", y);
+		
+		System.out.println("Digite um numero com casa decimal:");
+		z = sc.nextDouble(); // Atribuindo z como input/scanner (Para double nextDouble())
+		System.out.printf("Uau %.2f!!%n", z);
+		
+		System.out.println("Digite um nome:");
+		w = sc.next().charAt(0); // Atribuindo w como input/scanner (Para char sc.next().charAt(0))
+		System.out.printf("%s é a primeira letra do nome!%n", w);
+		
+		
+		//Ler os dados em uma única linha delimitadas por espaço
+		
+		String nome;
+		int idade;
+		double altura;
+		
+		nome = sc.next();
+		idade = sc.nextInt();
+		altura = sc.nextDouble();
+		
+		System.out.println(nome);
+		System.out.println(idade);
+		System.out.println(altura);
+		
+		sc.close(); //Ncessario para fechar o comando Scanner.
+	}
+
+}
