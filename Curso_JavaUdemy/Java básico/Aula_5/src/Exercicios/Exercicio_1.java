f33e5e043ec03e750be359cffb730ac8184fe61f
Caiopolis <caiohamoreira@gmail.com>
2026-01-28 17:12:25 -0300

    Adicionando exercicios

---
+++ Curso_JavaUdemy/Java básico/Aula_5/src/Exercicios/Exercicio_1.java
@@ -0,0 +1,26 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio_1 {
+
+	public static void main(String[] args) {
+
+		// Faça um programa para ler dois valores inteiros, e depois mostrar na tela a
+		// soma desses números com uma
+		// mensagem explicativa.
+
+		double x, y;
+		Scanner sc = new Scanner(System.in);
+		
+		
+		System.out.println("Digite um numero:");
+		x = sc.nextDouble();
+		System.out.println("Digite um numero:");
+		y = sc.nextDouble();
+		
+		System.out.printf("a soma de %.2f + %.2f = %.2f", x,y,x+y);
+
+	}
+
+}
---
+++ Curso_JavaUdemy/Java básico/Aula_5/src/Exercicios/Exercicio_2.java
@@ -0,0 +1,12 @@
+package Exercicios;
+
+public class Exercicio_2 {
+
+	public static void main(String[] args) {
+		
+		// Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro
+		//casas decimais conforme exemplos.
+
+	}
+
+}
