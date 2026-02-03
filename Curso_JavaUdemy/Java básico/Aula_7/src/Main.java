d208aa2612cf2a07061982bb9fec39d59be6774d
Caiopolis <caiohamoreira@gmail.com>
2026-02-02 23:32:10 -0300

    Merge branch 'master' of https://github.com/Caiopolis/Java--Developer.git
    Adicionando a aula 7

--- Curso_JavaUdemy/Java básico/Aula_7/src/Main.java
+++ Curso_JavaUdemy/Java básico/Aula_7/src/Main.java
@@ -14,7 +14,7 @@ public class Main {
 				Scanner sc = new Scanner(System.in);
 		
 				
-				System.out.println("Digite um numero");
+				System.out.println("Digite um numero: ");
 				x = sc.nextDouble();
 				
 				while(x != 0) {
